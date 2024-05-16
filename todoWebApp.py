ff2da839c5b0cae1f9527e7b2bb15452e807ca5e
faizan <faizanrawther2001@gmail.com>
2024-05-16 20:14:16 +0530

    implement add todo items

--- todoWebApp.py
+++ todoWebApp.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import functions as fc
 todos=fc.read_todos()
+def add_todo():
+    todo=st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    fc.write_todos(todos)
+
 st.title("My todo webapp")
 st.subheader("Lists")
 st.write("This app is makes your productivity")
-st.text_input(label="Enter a todo:  ",placeholder="Add a todo")
+st.text_input(label="Enter a todo:  ",placeholder="Add a todo",on_change=add_todo,key="new_todo")
 st.checkbox("Buy grocery")
 
 for todo in todos:
