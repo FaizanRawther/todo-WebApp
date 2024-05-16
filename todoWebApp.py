cf58d2e334225a2faf94c81c0379b5c15c57b5a6
faizan <faizanrawther2001@gmail.com>
2024-05-16 22:31:00 +0530

    file uploading to a server

--- todoWebApp.py
+++ todoWebApp.py
@@ -10,8 +10,13 @@ st.title("My todo webapp")
 st.subheader("Lists")
 st.write("This app is makes your productivity")
 st.text_input(label="Enter a todo:  ",placeholder="Add a todo",on_change=add_todo,key="new_todo")
-st.checkbox("Buy grocery")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    check_box=st.checkbox(todo,key=todo)
+    if check_box:
+        todos.pop(index)
+        fc.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun
+st.session_state
 
